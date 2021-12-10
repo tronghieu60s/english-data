46868140b915b367f3149e7bb08591a0424fa80d
Trong Hieu <tronghieu60s@gmail.com>
2021-12-10 19:35:25 +0700

    rename folder

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 
 /* 2000_Words_Topic */
 
-let Words600 = require("./600_Words_TOEIC/words.json");
-let Groups600 = require("./600_Words_TOEIC/groups.json");
+let Words600 = require("./1.600_Words_Toeic/words.json");
+let Groups600 = require("./1.600_Words_Toeic/groups.json");
 
 Words600 = Words600.map((o, i) => ({ ...o, id_word: `${i + 1}` }));
 Groups600 = Groups600.map((o, i) => ({ ...o, id_group: `${i + 1}` }));
 
 fs.writeFile(
-  "./600_Words_TOEIC/words.json",
+  "./1.600_Words_Toeic/words.json",
   JSON.stringify(Words600),
   "utf8",
   function (err) {
@@ -22,7 +22,7 @@ fs.writeFile(
 );
 
 fs.writeFile(
-  "./600_Words_TOEIC/groups.json",
+  "./1.600_Words_Toeic/groups.json",
   JSON.stringify(Groups600),
   "utf8",
   function (err) {
@@ -30,20 +30,20 @@ fs.writeFile(
       return console.log(err);
     }
 
-    console.log("600_Words_TOEIC: The file groups was saved!");
+    console.log("600_Words_Toeic: The file groups was saved!");
   }
 );
 
 /* 2000_Words_Topic */
 
-let Words2000 = require("./2000_Words_Topic/words.json");
-let Groups2000 = require("./2000_Words_Topic/groups.json");
+let Words2000 = require("./2.2000_Words_Topic/words.json");
+let Groups2000 = require("./2.2000_Words_Topic/groups.json");
 
 Words2000 = Words2000.map((o, i) => ({ ...o, id_word: `${i + 1}` }));
 Groups2000 = Groups2000.map((o, i) => ({ ...o, id_group: `${i + 1}` }));
 
 fs.writeFile(
-  "./2000_Words_Topic/words.json",
+  "./2.2000_Words_Topic/words.json",
   JSON.stringify(Words2000),
   "utf8",
   function (err) {
@@ -56,7 +56,7 @@ fs.writeFile(
 );
 
 fs.writeFile(
-  "./2000_Words_Topic/groups.json",
+  "./2.2000_Words_Topic/groups.json",
   JSON.stringify(Groups2000),
   "utf8",
   function (err) {
